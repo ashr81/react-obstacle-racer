d8a5b3c8489f5d2990fb14a3bc4e8eb7f3775236
ashr81 <ashrithreddi81@gmail.com>
2019-11-30 23:38:56 +0530

    Adds click functionality into game

--- src/customHooks/usePlayerPositionX.js
+++ src/customHooks/usePlayerPositionX.js
@@ -16,9 +16,26 @@ const usePlayerPositionX = () => {
         })
       }
     }
+    const onClickEventHandler = (event) => {
+      const windowWidth = window.innerWidth
+      if(windowWidth/2 <= event.clientX) {
+        updatePlayerPositionX(positionX => {
+          if(positionX < 200) return positionX + 100;
+          else return positionX;
+        })
+      } else {
+        updatePlayerPositionX(positionX => {
+          if(positionX >= 100) return positionX - 100;
+          else return positionX;
+        })
+      }
+    }
     window.addEventListener('keydown', onKeyPressEventHandler)
-    window.addEventListener('click', onKeyPressEventHandler)
-    window.addEventListener('touchend', onKeyPressEventHandler)
+    window.addEventListener('click', onClickEventHandler)
+    return () => {
+      window.removeEventListener('keydown', onKeyPressEventHandler)
+      window.removeEventListener('click', onClickEventHandler)
+    }
   }, [])
   return { playerPostionX }
 }
