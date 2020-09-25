e176a147844277ef4d86d3074a95ed1fc41fd0a6
ashr81 <ashrithreddi81@gmail.com>
2020-09-26 00:51:08 +0530

    using experimental react.

--- src/components/artifacts/index.js
+++ src/components/artifacts/index.js
@@ -1,11 +1,11 @@
 import { lazy } from 'react';
 
+const Flex = lazy(() => import('./Flex'));
 const ObstacleArt = lazy(() => import('./ObstacleArt'));
 const PlayerArt = lazy(() => import('./PlayerArt'));
-const Flex = lazy(() => import('./Flex'));
 const Text = lazy(() => import('./Text'));
 const GameOverArt = lazy(() => import('./GameOverArt'))
 
 export {
-  ObstacleArt, PlayerArt, Text, GameOverArt, Flex
+  Flex, ObstacleArt, PlayerArt, Text, GameOverArt
 }
